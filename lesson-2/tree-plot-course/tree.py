90b6e662976b572183d266125bda1da9f5fa8449
gzeureka <gzeureka@gmail.com>
2017-07-21 23:04:48 +0800

    prepare for homework

--- lesson-2/tree-plot-course/tree.py
+++ lesson-2/tree-plot-course/tree.py
@@ -8,7 +8,30 @@ class TreeNode(object):
     def isLeaf(self):
         return len(self.children) == 0
 
+    def getHeight(self):
+        if self.isLeaf():
+            return 1
+
+        return 1 + max([child.getHeight() for child in self.children.values()])
+
+    def getWidth(self):
+        if self.isLeaf():
+            return 1
+
+        return sum([child.getWidth() for child in self.children.values()])
 
 class Tree(object):
     def __init__(self, rootNode):
         self.rootNode = rootNode
+
+    def getHeight(self):
+        if self.rootNode is None:
+            return 0
+
+        return self.rootNode.getHeight()
+
+    def getWidth(self):
+        if self.rootNode is None:
+            return 0
+
+        return self.rootNode.getWidth()
